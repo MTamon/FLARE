0c360d61e896d3ff0e885d37fc7a66f7f83423ea
MTamon <mtamon.0044@gmail.com>
2026-03-19 02:40:17 +0900

    [add] re arrangement directory

--- __init__.py
+++
@@ -1,3 +0,0 @@
-"""FLARE: Facial Landmark Analysis & Rendering Engine リアルタイム特徴量抽出・レンダリングツール"""
-
-__version__ = "0.1.0"
---
+++ flare/__init__.py
@@ -0,0 +1,13 @@
+"""FLARE: Facial Landmark Analysis & Rendering Engine リアルタイム特徴量抽出・レンダリングツール"""
+
+from . import config
+from . import utils
+
+__copyright__ = "Copyright (C) 2023 Tamon Mikawa"
+__version__ = "0.1.0"
+__license__ = "MIT License"
+__author__ = "Tamon Mikawa"
+__author_email__ = "mtamon.engineering@gmail.com"
+__url__ = "https://github.com/MTamon/FLARE"
+
+__all__ = ["config", "utils"]
